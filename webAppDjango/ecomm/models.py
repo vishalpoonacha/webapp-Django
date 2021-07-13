44ca4238bd3864b2e803b761c5be03d326b26f83
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-07-13 23:53:33 +0530

    commit

--- webAppDjango/ecomm/models.py
+++ webAppDjango/ecomm/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ProductInformation(models.Model):
+    name = models.CharField(max_length=32)
+    price = models.FloatField()
+    category = models.CharField(max_length=32)
+    company = models.CharField(max_length=32)
+    quantity = models.IntegerField()
+    description = models.TextField(blank=True)
+
--- webAppDjango/webAppDjango/settings.py
+++ webAppDjango/webAppDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u6r5r22=$782ge1f^p1p-_-xb@+xfpm@*h##!&4&zk)zz&#vks'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecomm',
 ]
 
 MIDDLEWARE = [
